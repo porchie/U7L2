bb43b5c5ba2d5f56a06eeda6be54ee7384ae0331
kc227 <kc227155983@gmail.com>
2022-02-03 18:16:58 -0500

    Commit test

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class MainRunner {
     public static void main(String[] args) {
 
-        ArrayList<Integer> numList = new ArrayList<Integer>();
+        ArrayList<Integer> numList = new ArrayList<>();
+        System.out.println("what");
         
 
 
